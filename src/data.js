a10e3fc98df85b96dd481159ec32a8a856841313
Valeriia Humen <humen.valeriia@student.karazin.ua>
2026-05-18 18:58:34 +0300

    Complete MVP: product catalog, styles, and cart logic

---
+++ src/data.js
@@ -0,0 +1,140 @@
+export const flowers = [
+  {
+    id: 1,
+    name: "Бузкова симфонія",
+    price: 400,
+    image: "https://images.unsplash.com/photo-1672597121130-a7e9f549dcee?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8JUQwJUIxJUQxJTgzJUQwJUI3JUQwJUJFJUQwJUJBJTIwJUQwJUIxJUQxJTgzJUQwJUJBJUQwJUI1JUQxJTgyfGVufDB8fDB8fHww"
+  },
+  {
+    id: 2,
+    name: "Весняна гармонія",
+    price: 800,
+    image: "https://images.unsplash.com/photo-1748085901425-5121287df965?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8JUQxJTgyJUQxJThFJUQwJUJCJUQxJThDJUQwJUJGJUQwJUIwJUQwJUJEJUQwJUI4fGVufDB8fDB8fHww"
+  },
+  {
+    id: 3,
+    name: "Магія квітів",
+    price: 750,
+    image: "https://plus.unsplash.com/premium_photo-1676475964992-6404b8db0b53?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Zmxvd2VyfGVufDB8fDB8fHww"
+  },
+  {
+    id: 4,
+    name: "Ніжність",
+    price: 990,
+    image: "https://images.unsplash.com/photo-1710629357740-546115c6f75b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fFJvc2V8ZW58MHx8MHx8fDA%3D"
+    },
+  {
+    id: 5,
+    name: "Тепло душі",
+    price: 200,
+    image: "https://images.unsplash.com/photo-1718043359450-800b6f862122?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU2fHxDaGFtb21pbGV8ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    id: 6,
+    name: "Сяйво",
+    price: 300,
+    image: "https://images.unsplash.com/photo-1560881088-30da8d6e1378?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fFN1bmZsb3dlcnxlbnwwfHwwfHx8MA%3D%3D"
+  },
+  {
+    id: 7,
+    name: "Вишуканість",
+    price: 450,
+    image: "https://images.unsplash.com/photo-1621154089496-ba8c17a77486?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fExpbHl8ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    id: 8,
+    name: "Емоції",
+    price: 700,
+    image: "https://images.unsplash.com/photo-1511201173873-c327e63eb6c4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8UGVvbnl8ZW58MHx8MHx8fDA%3D"
+    },
+  {
+    id: 9,
+    name: "Турбота",
+    price: 650,
+    image: "https://images.unsplash.com/photo-1618080578815-335456280012?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fE9yY2hpZHxlbnwwfHwwfHx8MA%3D%3D"
+  },
+  {
+    id: 10,
+    name: "Фіолетовий настрій",
+    price: 250,
+    image: "https://plus.unsplash.com/premium_photo-1675712535056-fc6fa6aee8ec?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8VmlvbGV0fGVufDB8fDB8fHww"
+  },
+  {
+    id: 11,
+    name: "Серце поля",
+    price: 100,
+    image: "https://images.unsplash.com/photo-1589820546347-ec06b066b6ff?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fFBvcHB5fGVufDB8fDB8fHww"
+    },  
+  {
+    id: 12,
+    name: "Спокій",
+    price: 350,
+    image: "https://images.unsplash.com/photo-1612380635121-411eda9ecbb9?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8SmFzbWluZXxlbnwwfHwwfHx8MA%3D%3D"
+  },
+  {
+    id: 13,
+    name: "Гармонія кольорів",
+    price: 550,
+    image: "https://images.unsplash.com/photo-1538059084574-395f140cccef?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzB8fENocnlzYW50aGVtdW18ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    id: 14,
+    name: "Сонячний ранок",
+    price: 150,
+    image: "https://images.unsplash.com/photo-1485431142439-206ba3a9383e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8RGFmZm9kaWx8ZW58MHx8MHx8fDA%3D"
+    }, 
+  {
+    id: 15,
+    name: "Перлинки",
+    price: 50,
+    image: "https://images.unsplash.com/photo-1621336938698-f0070cc4c3bd?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fExpbHklMjBvZiUyMHRoZSUyMHZhbGxleXxlbnwwfHwwfHx8MA%3D%3D"
+  },
+  {
+    id: 16,
+    name: "Айстрова елегія",
+    price: 120,
+    image: "https://images.unsplash.com/photo-1631434382267-5aba6f8f9334?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fEFzdGVyfGVufDB8fDB8fHww"
+  },
+  {
+    id: 17,
+    name: "Щирість",
+    price: 110,
+    image: "https://images.unsplash.com/photo-1617265178200-facb3f9d8f37?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q2FybmF0aW9ufGVufDB8fDB8fHww"
+    },
+  {
+    id: 18,
+    name: "Ірисова мрія",
+    price: 250,
+    image: "https://images.unsplash.com/photo-1621725322096-2c8b92b0b1b1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fElyaXN8ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    id: 19,
+    name: "Аромат спокою",
+    price: 400,
+    image: "https://images.unsplash.com/photo-1565011523534-747a8601f10a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fExhdmVuZGVyfGVufDB8fDB8fHww"
+  },
+  {
+    id: 20,
+    name: "Дотик природи",
+    price: 515,
+    image: "https://plus.unsplash.com/premium_photo-1713823800827-4c10d4d37585?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Zmxvd2VyJTIwYm91cXVldHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+  {
+    id: 21,
+    name: "Хаос",
+    price: 650,
+    image: "https://images.unsplash.com/photo-1604323990536-e5452c0507c1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGZsb3dlciUyMGJvdXF1ZXR8ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    id: 22,
+    name: "Тепло сім'ї",
+    price: 420,
+    image: "https://images.unsplash.com/photo-1689085055401-2e6a7268d6ca?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzZ8fGZsb3dlciUyMGJvdXF1ZXR8ZW58MHx8MHx8fDA%3D"
+  },
+  {
+    id: 23,
+    name: "Різнобарв’я",
+    price: 314,
+    image: "https://images.unsplash.com/photo-1621581314652-06a9fe9d4db6?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTc0fHxmbG93ZXIlMjBib3VxdWV0fGVufDB8fDB8fHww"
+    }
+];
